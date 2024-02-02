c15359f900101065092a793e34948985e3dcfe1c
fabioaspssos <fabioaspassos@gmail.com>
2024-02-02 14:01:28 +0000

    post create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,6 +2,10 @@ echo 'Instaling Gems...'
 cd /workspaces/sample_rails_app
 bundle install
 
+echo 'Starting Database...'
+rails db:create
+rails db:migrate
+
 echo 'Yarn Install...'
 yarn install --check-files
 
